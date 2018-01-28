4280c5e2e891f3bef0ae6ec1d1b792124701d184
Elle Faraday <faraday.elle@gmail.com>
2018-01-28 03:24:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,18 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :new
+    end
 
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      
+      params[:pirates][:ships].each do |details|
+        Pirate.new(details)
+      end
+      
+      erb :show
+    end
   end
 end
