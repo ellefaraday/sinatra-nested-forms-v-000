40a6f3581c8e1bf51d8af3a91dc304eccf4bc38f
Elle Faraday <faraday.elle@gmail.com>
2018-01-28 03:21:25 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,6 +2,12 @@ class Ship
   attr_accessor :name, :type, :booty
   @@SHIPS = []
 
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+  end
+
   def self.all
     @@SHIPS
   end
